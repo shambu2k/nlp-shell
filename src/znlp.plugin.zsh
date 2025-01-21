5c45422c621fdf57fa399a7740620fa982024d24
Sidharth Shambu <sshambu@MacBook-Pro.local>
2025-01-20 20:30:13 -0500

    fix: setup issue, znlp runs from anywhere now

--- src/znlp.plugin.zsh
+++ src/znlp.plugin.zsh
@@ -5,7 +5,9 @@ function znlp() {
   description=$(echo $description | sed "s/'//g" | sed "s/\"//g" | sed "s/\\\\//g")
 
   local os_type=$(uname)
-  local command=$(node -e "(async () => { const { getCommand } = await import('./src/llm.js'); console.log(await getCommand('$description', '$os_type')); })();")
+  local script_path=${${(%):-%x}:A:h}
+  local project_dir=$(realpath $script_path/..)
+  local command=$(node -e "(async () => { const { getCommand } = await import('$project_dir/src/llm.js'); console.log(await getCommand('$description', '$os_type')); })();")
 
   echo "Description: $description"
   echo "Generated command: $command"
