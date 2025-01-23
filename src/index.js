1ac1fd2b19b91c15f5be2888eb600116f24a971f
shambu2k <sidharthshambu00@gmail.com>
2025-01-22 19:45:24 -0500

    chore: version bump

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 
 import { getCommand } from './llm.js';
 import readline from 'readline';
-import { exec } from 'child_process';
+import { spawn } from 'child_process';
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -20,17 +20,8 @@ const osType = process.platform === 'win32' ? 'Windows' : 'Linux';
 
   rl.question('Do you want to execute this command? (y/n) ', (answer) => {
     if (answer.toLowerCase() === 'y') {
-      exec(command, (error, stdout, stderr) => {
-        if (error) {
-          console.error(`Error executing command: ${error.message}`);
-          return;
-        }
-        if (stderr) {
-          console.error(`${stderr}`);
-          return;
-        }
-        console.log(`${stdout}`);
-      });
+      const [cmd, ...args] = command.split(' ');
+      spawn(cmd, args, { stdio: 'inherit', shell: true });
     } else {
       console.log('Command not executed.');
     }
